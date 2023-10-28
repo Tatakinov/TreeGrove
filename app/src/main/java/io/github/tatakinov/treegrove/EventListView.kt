a7c7b6814a524ebbb1ff9ede4fbcafbad751a11a
Tatakinov <tatakinov@gmail.com>
2023-10-28 15:56:34 +0900

    書き込み時にlazycolumnがtextfield分移動するようにした。

--- app/src/main/java/io/github/tatakinov/treegrove/EventListView.kt
+++ app/src/main/java/io/github/tatakinov/treegrove/EventListView.kt
@@ -1,6 +1,7 @@
 package io.github.tatakinov.treegrove
 
 import android.widget.Toast
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
@@ -18,6 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import fr.acinq.secp256k1.Hex
@@ -53,7 +55,16 @@ fun EventListView(onGetPostDataList : () -> List<EventData>,
     var replyEvent : Event? by remember {
         mutableStateOf(null)
     }
-    LazyColumn(state = onGetLazyListState(), modifier = modifier) {
+    var height by remember { mutableStateOf(0) }
+    LazyColumn(state = onGetLazyListState(), modifier = modifier.onGloballyPositioned {
+        if (height != it.size.height) {
+            val diffHeight = height - it.size.height
+            height = it.size.height
+            scope.launch(Dispatchers.Main) {
+                onGetLazyListState().scrollBy(diffHeight.toFloat())
+            }
+        }
+    }) {
         items(items = onGetPostDataList(), key = { it.event.toJSONObject().toString() }) { event ->
             EventView(event.event, onGetEventMap = onGetEventMap,
                 onGetChannelMetaData = onGetChannelMetaData,
